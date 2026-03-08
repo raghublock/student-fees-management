41b21469e9847ad90fccb924a4cb9da3acd22b9b
raghu <149569406+raghublock@users.noreply.github.com>
2026-03-08 19:13:10 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,23 +1,23 @@
 // src/config.js
 
 const config = {
-
    // 🔗 Backend API Link (Sabse Zaroori)
-  apiUrl: "https://gym-api.raghuveerbhati525.workers.dev",
+  apiUrl: "https://library-api.raghuveerbhati525.workers.dev",
+
   // Business Details
-  appName: "Body GYM",       // Gym ke liye: "FitLife Gym"
+  appName: "Laxmi Library",       // Gym ke liye: "FitLife Gym"
   branchName: "Bikaner Branch",   // Gym ke liye: "JNV Colony Branch"
-  mainEmoji: "🏋️",                // Gym ke liye: "🏋️"
+  mainEmoji: "📚",                // Gym ke liye: "🏋️"
   
   // User Terminology
-  userType: "Member",            // Gym ke liye: "Member"
-  userTypePlural: "Members",     // Gym ke liye: "Members"
+  userType: "Student",            // Gym ke liye: "Member"
+  userTypePlural: "Students",     // Gym ke liye: "Members"
   
   // Plan Terminology
-  planLabel: "Package",              // Gym ke liye: "Package"
+  planLabel: "Plan",              // Gym ke liye: "Package"
   
   // Receipt Prefix
-  receiptPrefix: "FIT",            // Gym ke liye: "FIT"
+  receiptPrefix: "LX",            // Gym ke liye: "FIT"
 };
 
 export default config;
