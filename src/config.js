894aba16d31d54a0022fae96c5f42d7fdc8b0d7f
raghu <149569406+raghublock@users.noreply.github.com>
2026-03-08 18:26:02 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,19 +2,19 @@
 
 const config = {
   // Business Details
-  appName: "Body GYM",       // Gym ke liye: "FitLife Gym"
+  appName: "Laxmi Library",       // Gym ke liye: "FitLife Gym"
   branchName: "Bikaner Branch",   // Gym ke liye: "JNV Colony Branch"
-  mainEmoji: "🏋️",                // Gym ke liye: "🏋️"
+  mainEmoji: "📚",                // Gym ke liye: "🏋️"
   
   // User Terminology
-  userType: "Member",            // Gym ke liye: "Member"
-  userTypePlural: "Members",     // Gym ke liye: "Members"
+  userType: "Student",            // Gym ke liye: "Member"
+  userTypePlural: "Students",     // Gym ke liye: "Members"
   
   // Plan Terminology
-  planLabel: "Package",              // Gym ke liye: "Package"
+  planLabel: "Plan",              // Gym ke liye: "Package"
   
   // Receipt Prefix
-  receiptPrefix: "FIT",            // Gym ke liye: "FIT"
+  receiptPrefix: "LX",            // Gym ke liye: "FIT"
 };
 
 export default config;
